86b2e3744edc709acb56103e94ffd18341049f58
Ratchakorns <47313416+Ratchakorns@users.noreply.github.com>
2019-05-05 06:13:30 +0700

    Update Splashscreen & Icon App (Android)

--- lib/screen/homescreen.dart
+++ lib/screen/homescreen.dart
@@ -31,139 +31,132 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       Scaffold(
-          appBar: AppBar(
-            title: GradientText("North-West Corner Rule",
-                gradient: LinearGradient(colors: [
-                  Color(0xFFD16BA5),
-                  Color(0xFF86A8E7),
-                  Color(0xFF5fb7fb),
-                ]),
-                style: TextStyle(
-                  fontFamily: 'PromptBold',
-                )),
-            centerTitle: true,
-            backgroundColor: Colors.transparent,
-            elevation: 0.0,
-          ),
           backgroundColor: Colors.transparent,
-          body: Padding(
-              padding: EdgeInsets.all(0),
+          body: Center(
               child: Container(
-                child: SingleChildScrollView(
-                  child: Container(
-                      child: Padding(
-                          padding:
-                              EdgeInsets.only(left: 20, right: 20, bottom: 20),
-                          child: Column(
-                            children: <Widget>[
-                              Text(
-                                'เป็นวิธีการหาคำเฉลยเบื้องต้นที่ง่ายที่สุด วิธีการหาคำเฉลยจะเริ่มจากช่องมุมบนซ้ายที่สุดของตารางเสมอ และจะต้องจัดสรรปริมาณที่จะขนส่งไปตามเงื่อนไขของแถวนอนหรือแถวตั้งแรกเสียก่อน แล้วจึงย้ายไปพิจารณาแถวนอนและแถวตั้งถัดไป จนกระทั่งครบตามเงื่อนไขของแถวนอน และ แถวตั้ง',
-                                style: TextStyle(fontSize: 16),
-                              ),
-                              Padding(
-                                padding: EdgeInsets.only(top: 20),
-                                child: Card(
-                                    child: InkWell(
-                                  onTap: () {},
-                                  child: Container(
-                                    child: Column(
-                                      mainAxisSize: MainAxisSize.min,
-                                      children: <Widget>[
-                                        const ListTile(
-                                          title: Text(
-                                            '2X2',
-                                            style: TextStyle(fontSize: 24),
-                                          ),
-                                          subtitle: Text(
-                                              'สำหรับ Supply 2 แถว Demand 2 แถว'),
-                                        ),
-                                      ],
+            child: SingleChildScrollView(
+              child: Container(
+                  child: Padding(
+                      padding: EdgeInsets.only(
+                          left: 30, right: 30, bottom: 20, top: 40),
+                      child: Column(
+                        children: <Widget>[
+                          GradientText("North-West Corner Rule",
+                              gradient: LinearGradient(colors: [
+                                Color(0xFFD16BA5),
+                                Color(0xFF86A8E7),
+                                Color(0xFF5fb7fb),
+                              ]),
+                              style: TextStyle(
+                                  fontFamily: 'PromptBold', fontSize: 36)),
+                          Text(
+                            'เป็นวิธีการหาคำเฉลยเบื้องต้นที่ง่ายที่สุด วิธีการหาคำเฉลยจะเริ่มจากช่องมุมบนซ้ายที่สุดของตารางเสมอ และจะต้องจัดสรรปริมาณที่จะขนส่งไปตามเงื่อนไขของแถวนอนหรือแถวตั้งแรกเสียก่อน แล้วจึงย้ายไปพิจารณาแถวนอนและแถวตั้งถัดไป จนกระทั่งครบตามเงื่อนไขของแถวนอน และ แถวตั้ง',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(top: 20),
+                            child: Card(
+                                child: InkWell(
+                              onTap: () {},
+                              child: Container(
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: <Widget>[
+                                    const ListTile(
+                                      title: Text(
+                                        '2X2',
+                                        style: TextStyle(fontSize: 24),
+                                      ),
+                                      subtitle: Text(
+                                          'สำหรับ Supply 2 แถว Demand 2 แถว'),
                                     ),
-                                  ),
-                                )),
+                                  ],
+                                ),
                               ),
-                              Card(
-                                  child: InkWell(
-                                onTap: () {},
-                                child: Container(
-                                  child: Column(
-                                    mainAxisSize: MainAxisSize.min,
-                                    children: <Widget>[
-                                      const ListTile(
-                                        title: Text(
-                                          '2X3',
-                                          style: TextStyle(fontSize: 24),
-                                        ),
-                                        subtitle: Text(
-                                            'สำหรับ Supply 2 แถว Demand 3 แถว'),
-                                      ),
-                                    ],
+                            )),
+                          ),
+                          Card(
+                              child: InkWell(
+                            onTap: () {},
+                            child: Container(
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: <Widget>[
+                                  const ListTile(
+                                    title: Text(
+                                      '2X3',
+                                      style: TextStyle(fontSize: 24),
+                                    ),
+                                    subtitle: Text(
+                                        'สำหรับ Supply 2 แถว Demand 3 แถว'),
                                   ),
-                                ),
-                              )),
-                              Card(
-                                  child: InkWell(
-                                onTap: () {
-                                  Navigator.pushNamed(context, '/3x3');
-                                },
-                                child: Container(
-                                  child: Column(
-                                    mainAxisSize: MainAxisSize.min,
-                                    children: <Widget>[
-                                      const ListTile(
-                                        title: Text(
-                                          '3X3',
-                                          style: TextStyle(fontSize: 24),
-                                        ),
-                                        subtitle: Text(
-                                            'สำหรับ Supply 3 แถว Demand 3 แถว'),
-                                      ),
-                                    ],
+                                ],
+                              ),
+                            ),
+                          )),
+                          Card(
+                              child: InkWell(
+                            onTap: () {
+                              Navigator.pushNamed(context, '/3x3');
+                            },
+                            child: Container(
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: <Widget>[
+                                  const ListTile(
+                                    title: Text(
+                                      '3X3',
+                                      style: TextStyle(fontSize: 24),
+                                    ),
+                                    subtitle: Text(
+                                        'สำหรับ Supply 3 แถว Demand 3 แถว'),
                                   ),
-                                ),
-                              )),
-                              Card(
-                                  child: InkWell(
-                                onTap: () {},
-                                child: Container(
-                                  child: Column(
-                                    mainAxisSize: MainAxisSize.min,
-                                    children: <Widget>[
-                                      const ListTile(
-                                        title: Text(
-                                          '3X4',
-                                          style: TextStyle(fontSize: 24),
-                                        ),
-                                        subtitle: Text(
-                                            'สำหรับ Supply 3 แถว Demand 4 แถว'),
-                                      ),
-                                    ],
+                                ],
+                              ),
+                            ),
+                          )),
+                          Card(
+                              child: InkWell(
+                            onTap: () {},
+                            child: Container(
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: <Widget>[
+                                  const ListTile(
+                                    title: Text(
+                                      '3X4',
+                                      style: TextStyle(fontSize: 24),
+                                    ),
+                                    subtitle: Text(
+                                        'สำหรับ Supply 3 แถว Demand 4 แถว'),
                                   ),
-                                ),
-                              )),
-                              Card(
-                                  child: InkWell(
-                                onTap: () {},
-                                child: Container(
-                                  child: Column(
-                                    mainAxisSize: MainAxisSize.min,
-                                    children: <Widget>[
-                                      const ListTile(
-                                        title: Text(
-                                          '4X4',
-                                          style: TextStyle(fontSize: 24),
-                                        ),
-                                        subtitle: Text(
-                                            'สำหรับ Supply 4 แถว Demand 4 แถว'),
-                                      ),
-                                    ],
+                                ],
+                              ),
+                            ),
+                          )),
+                          Card(
+                              child: InkWell(
+                            onTap: () {},
+                            child: Container(
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: <Widget>[
+                                  const ListTile(
+                                    title: Text(
+                                      '4X4',
+                                      style: TextStyle(fontSize: 24),
+                                    ),
+                                    subtitle: Text(
+                                        'สำหรับ Supply 4 แถว Demand 4 แถว'),
                                   ),
-                                ),
-                              )),
-                            ],
-                          ))),
-                ),
-              ))),
+                                ],
+                              ),
+                            ),
+                          )),
+                        ],
+                      ))),
+            ),
+          ))),
     ]);
   }
 }
