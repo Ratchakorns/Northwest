582a6d4db60a6f9a22b25bced2079fd97044fb7e
Ratchakorns <47313416+Ratchakorns@users.noreply.github.com>
2019-05-05 21:59:32 +0700

    Add 2x2 & 2x3 Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:northwest/screen/2x2.dart';
+import 'package:northwest/screen/2x3.dart';
 import 'package:northwest/screen/3x3.dart';
 import 'package:northwest/screen/3x4.dart';
 import 'package:northwest/screen/4x4.dart';
@@ -17,6 +19,8 @@ class MyApp extends StatelessWidget {
       ),
       home: MyHomePage(),
       routes: {
+        '/2x2': (BuildContext context) => Northwest2x2(title: 'NorthWest'),
+        '/2x3': (BuildContext context) => Northwest2x3(title: 'NorthWest'),
         '/3x3': (BuildContext context) => Northwest3x3(title: 'NorthWest'),
         '/3x4': (BuildContext context) => Northwest3x4(title: 'NorthWest'),
         '/4x4': (BuildContext context) => Northwest4x4(title: 'NorthWest'),
